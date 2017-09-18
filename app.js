379861d25fcbb7f676d0586046b2e84f996b313a
zhitongLIU <zhitonggm.liu@gmail.com>
2017-09-19 00:09:09 +0200

    Add loop for changing images

--- app.js
+++ app.js
@@ -1,20 +1,46 @@
-$(document).ready( function() {
-  //first one is the one we need
-  var keyword = "europe";
+var interval = 5000;
+var base_url = "https://api.flickr.com/services/feeds/photos_public.gne?tagmode=any&format=json&tags=";
 
+$(document).ready( function() {
+  var keyword = "europe";
+  var element_identifier = '.hero-panel';
+  loop_random_image(element_identifier, keyword);
+});
+
+function loop_random_image(element_identifier, keyword) {
   var xhr = new XMLHttpRequest();
-  xhr.open("GET", "https://api.flickr.com/services/feeds/photos_public.gne?tags=europe&tagmode=any&format=json&_=1505690276918", true);
+  var url = base_url + keyword;
+  xhr.open("GET", url, true);
   xhr.onreadystatechange = function() {
-        if (xhr.readyState == 4) {
-          var resp = xhr.responseText.replace('jsonFlickrFeed(', '');
-          resp = resp.slice(0, -1);
-          var data = JSON.parse(resp);
-          var rnd = Math.floor(Math.random() * data.items.length);
-          var image_src = data.items[rnd]['media']['m'].replace("_m", "_b");
-          var panel = document.getElementsByClassName('hero-panel')[0];
-          $('.hero-panel').css('transition', "background 1s linear");
-          $('.hero-panel').css('background-image', "url('" + image_src + "')");
-        }
+     if (xhr.readyState == 4) {
+       var resp = clean_json_response(xhr.responseText);
+       var data = JSON.parse(resp);
+       loop_change_bg(element_identifier, data);
+     }
   }
   xhr.send();
-});
+}
+
+function clean_json_response(res) {
+  res = res.replace('jsonFlickrFeed(', '');
+  res = res.replace(/\uFFFD/g, '');
+  res = res.slice(0, -1);
+  return res;
+}
+
+function change_bg_image(element_identifier, image_src) {
+  // use get class because of trainline use class for hero-panel
+  var panel = document.getElementsByClassName(element_identifier.replace('\.', ''))[0];
+  // $(element_identifier).css('transition', "background 1s linear");
+  $(element_identifier).css('background-image', "url('" + image_src + "')");
+}
+
+function loop_change_bg(element_identifier, data) {
+  var rnd, image_src;
+  setTimeout(function () {
+    rnd = Math.floor(Math.random() * data.items.length);
+    image_src = data.items[rnd]['media']['m'].replace("_m", "_b");
+    change_bg_image(element_identifier, image_src);
+    loop_change_bg(element_identifier, data);
+  }, interval);
+}
